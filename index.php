3b3bd835c674005e5640324b177ac75efc7998f6
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-05-23 21:13:59 +0100

    update

--- index.php
+++ index.php
@@ -32,6 +32,13 @@
 				<div class="resp-tabs-container">
 					<div class="tab-1 resp-tab-content roundtrip">
 						<form action="#" method="post">
+
+						<div class="from">
+								<h3>From</h3>
+								<input type="email" name="email" class="email" placeholder="Email" required="">
+							</div>
+
+
 							<div class="from">
 								<h3>From</h3>
 								<input type="text" name="city" class="city1" placeholder="Type Departure City" required="">
@@ -93,68 +100,68 @@
 								<div class="clear"></div>
 							</div>
 							<div class="clear"></div>
-              <input type="submit" value="Search Flights" 
-              onclick="alert('Your query has been submitted. Please check your email.');">
-						</form>						
-					</div>		
-					<div class="tab-1 resp-tab-content oneway">
-						<form action="#" method="post">
-							<div class="from">
-								<h3>From</h3>
-								<input type="text" name="city" class="city1" placeholder="Type Departure City" required="">
-							</div>
-							<div class="to">
-								<h3>To</h3>
-								<input type="text" name="city" class="city2" placeholder="Type Destination City" required="">
-							</div>
-							<div class="clear"></div>
-							<div class="date">
-								<div class="depart">
-									<h3>Depart</h3>
-									<input class="date" id="datepicker2" name="Text" type="text" value="mm/dd/yyyy" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'mm/dd/yyyy';}" required="">
-									<span class="checkbox1">
-										<label class="checkbox"><input type="checkbox" name="" checked=""><i> </i>Flexible with date</label>
-									</span>
-								</div>
-							</div>
-							<div class="class">
-								<h3>Class</h3>
-								<select id="w3_country1" onchange="change_country(this.value)" class="frm-field required">
-									<option value="null">Economy</option>  
-									<option value="null">Premium Economy</option>   
-									<option value="null">Business</option>   
-									<option value="null">First class</option>   						
-								</select>
+								<input type="submit" value="Search Flights" 
+								onclick="alert('Your query has been submitted. Please check your email.');">
+											</form>						
+										</div>		
+										<div class="tab-1 resp-tab-content oneway">
+											<form action="#" method="post">
+												<div class="from">
+													<h3>From</h3>
+													<input type="text" name="city" class="city1" placeholder="Type Departure City" required="">
+												</div>
+												<div class="to">
+													<h3>To</h3>
+													<input type="text" name="city" class="city2" placeholder="Type Destination City" required="">
+												</div>
+												<div class="clear"></div>
+												<div class="date">
+													<div class="depart">
+														<h3>Depart</h3>
+														<input class="date" id="datepicker2" name="Text" type="text" value="mm/dd/yyyy" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'mm/dd/yyyy';}" required="">
+														<span class="checkbox1">
+															<label class="checkbox"><input type="checkbox" name="" checked=""><i> </i>Flexible with date</label>
+														</span>
+													</div>
+												</div>
+												<div class="class">
+													<h3>Class</h3>
+													<select id="w3_country1" onchange="change_country(this.value)" class="frm-field required">
+														<option value="null">Economy</option>  
+														<option value="null">Premium Economy</option>   
+														<option value="null">Business</option>   
+														<option value="null">First class</option>   						
+													</select>
 
-							</div>
-							<div class="clear"></div>
-							<div class="numofppl">
-								<div class="adults">
-									<h3>Adult:(12+ yrs)</h3>
-									<div class="quantity"> 
-										<div class="quantity-select">                           
-											<div class="entry value-minus">&nbsp;</div>
-											<div class="entry value"><span>1</span></div>
-											<div class="entry value-plus active">&nbsp;</div>
-										</div>
-									</div>
-								</div>
-								<div class="child">
-									<h3>Child:(2-11 yrs)</h3>
-									<div class="quantity"> 
-										<div class="quantity-select">                           
-											<div class="entry value-minus">&nbsp;</div>
-											<div class="entry value"><span>1</span></div>
-											<div class="entry value-plus active">&nbsp;</div>
-										</div>
-									</div>
-								</div>
-								<div class="clear"></div>
-							</div>
-							<div class="clear"></div>
-						  <input type="submit" value="Search Flights" 
-              onclick="alert('Your query has been submitted. Please check your email.');">
-						</form>	
+												</div>
+												<div class="clear"></div>
+												<div class="numofppl">
+													<div class="adults">
+														<h3>Adult:(12+ yrs)</h3>
+														<div class="quantity"> 
+															<div class="quantity-select">                           
+																<div class="entry value-minus">&nbsp;</div>
+																<div class="entry value"><span>1</span></div>
+																<div class="entry value-plus active">&nbsp;</div>
+															</div>
+														</div>
+													</div>
+													<div class="child">
+														<h3>Child:(2-11 yrs)</h3>
+														<div class="quantity"> 
+															<div class="quantity-select">                           
+																<div class="entry value-minus">&nbsp;</div>
+																<div class="entry value"><span>1</span></div>
+																<div class="entry value-plus active">&nbsp;</div>
+															</div>
+														</div>
+													</div>
+													<div class="clear"></div>
+												</div>
+												<div class="clear"></div>
+											<input type="submit" value="Search Flights" 
+								onclick="alert('Your query has been submitted. Please check your email.');">
+											</form>	
 								
 					</div>
 					<div class="tab-1 resp-tab-content multicity">
